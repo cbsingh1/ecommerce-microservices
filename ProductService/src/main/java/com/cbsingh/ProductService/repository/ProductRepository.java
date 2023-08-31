c19f919f85b255068df52d716c07bfd6d6186f52
Chandra Bhan Singh <cbhansingh1@gmail.com>
2023-08-31 20:40:33 +0800

    Added Product Microservice

---
+++ ProductService/src/main/java/com/cbsingh/ProductService/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cbsingh.ProductService.repository;
+
+import com.cbsingh.ProductService.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
