1061dd4356c7778bec23d39e538e6bbf133d529e
Chandra Bhan Singh <cbhansingh1@gmail.com>
2023-09-05 15:10:44 +0800

    Added config server to fetch common config from Github

---
+++ ConfigServer/src/test/java/com/cbsingh/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbsingh.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
